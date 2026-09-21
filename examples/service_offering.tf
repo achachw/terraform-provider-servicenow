a4850cc8bdd60c6c77a7a82edfe555a0c8740286
Walid ACHACH <walid.achach@canal-plus.com>
2026-09-21 11:11:37 +0200

    Simplify examples

--- examples/service_offering.tf
+++ examples/service_offering.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    servicenow = {
-      source = "achachw/servicenow"
-    }
-  }
-}
-
-provider "servicenow" {
-  base_url = "https://example.service-now.com"
-  # username can be set with SERVICENOW_USERNAME
-  # password can be set with SERVICENOW_PASSWORD
-}
-
 data "servicenow_service_offering" "example" {
   name = "Example Service Offering"
   view = "automate"
@@ -21,10 +7,3 @@ data "servicenow_user_group" "example" {
   name = "Example User Group"
 }
 
-output "service_offering_sys_id" {
-  value = data.servicenow_service_offering.example.sys_id
-}
-
-output "user_group_sys_id" {
-  value = data.servicenow_user_group.example.sys_id
-}
