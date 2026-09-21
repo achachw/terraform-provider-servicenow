17f7493f5bf932e43315708c9026bc4ce4483518
Walid ACHACH <walid.achach@canal-plus.com>
2026-09-21 11:10:47 +0200

    update

---
+++ examples/provider.tf
@@ -0,0 +1,5 @@
+provider "servicenow" {
+  base_url = "https://example.service-now.com"
+  # username can be set with SERVICENOW_USERNAME
+  # password can be set with SERVICENOW_PASSWORD
+}
---
+++ examples/user_group.tf
@@ -0,0 +1,7 @@
+output "service_offering_sys_id" {
+  value = data.servicenow_service_offering.example.sys_id
+}
+
+output "user_group_sys_id" {
+  value = data.servicenow_user_group.example.sys_id
+}
